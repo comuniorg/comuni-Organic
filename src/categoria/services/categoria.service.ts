2425f1e4df0061cc086b2805952c9ac367da8ccd
PauloCMoreira <pauloskx9004@gmail.com>
2022-10-19 09:04:28 -0300

    crud

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -35,7 +35,10 @@ export class categoriaService{
     async findByNome(categoria: string): Promise<Categoria[]>{
         return await this.categoriaRepository.find({
             where: {
-                categoria: ILike(`${categoria}`)
+                categoria: ILike(`%${categoria}%`)
+            },
+            relations: {
+                produto: true
             }
         });
     }
