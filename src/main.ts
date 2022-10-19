b99de6e0e8a9f2941cb6f48997296d4ff100bd5e
Jean gomes da silva <jean118877@hotmail.com>
2022-10-19 15:21:38 -0300

    documentacao main

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = 4000
 
+/**
+ * @desc
+ * a função está criando uma porta para o sistema conseguir rodar.
+ * também configurou o horário de brasilia, validation para rodar de forma global e Habilita que o computador
+ * receba requisições de fora.
+ */
 async function bootstrap() {
+  const port = 4000;
+
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ = '-3:00';
