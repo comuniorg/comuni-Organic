6d201d7bd8923698d0a65bb52bdde47d3e418efb
hugotank99 <hugoramosjan@gmail.com>
2022-10-27 15:48:21 -0300

    Criação do auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { UsuarioModule } from "src/usuario/usuario.module";
+import { Bcrypt } from "./bcrypt/bcrypt";
+import { jwtConstants } from "./constants/auth.constants";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
+import { JwtStrategy } from "./strategy/jwt.strategy";
+import { LocalStrategy } from "./strategy/local.strategy";
+
+@Module({
+    imports:[
+        UsuarioModule,
+        PassportModule,
+        JwtModule.register({
+            secret: jwtConstants.secret,
+            signOptions: {expiresIn:'24h'}
+        })
+    ],
+    providers:[
+        Bcrypt,
+        AuthService,
+        LocalStrategy,
+        JwtStrategy
+    ],
+    controllers:[AuthController],
+    exports:[Bcrypt]
+})
+export class AuthModule { }
