774642b67ff8b3381b24ca36372b363a13c4bc60
Murilo Nascimento <murilojesus659@gmail.com>
2022-10-27 09:47:01 -0300

    feat: add auth.module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { Bcrypt } from "./bcrypt/bcrypt";
+
+@Module({
+    imports: [],
+    providers: [Bcrypt],
+    controllers: [],
+    exports: [Bcrypt]
+})
+export class AuthModule {}
