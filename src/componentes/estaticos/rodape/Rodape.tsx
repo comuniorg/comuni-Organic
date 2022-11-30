edf07169826af54519af983042da4bc1cee88827
Jean Gomes <jean118877@hotmail.com>
2022-11-30 19:37:02 -0300

    incompleto

---
+++ src/componentes/estaticos/rodape/Rodape.tsx
@@ -0,0 +1,28 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import github from '../../../assets/images/github.svg';
+import discord from '../../../assets/images/discord.svg';
+import "./Rodape.css";
+
+function Rodape(){
+    return(
+      <>
+        <Grid container direction="row" justifyContent="space-evenly">
+          <Grid item container xs={6} justifyContent="space-evenly">
+            <Grid item>
+              <a href="#">
+                <img src={github} alt="Discord"/>
+              </a>
+            </Grid>
+            <Grid>
+              <a href="#">
+                <img src={discord} alt="Discord"/>
+              </a>
+            </Grid>
+          </Grid>
+        </Grid>
+      </>
+    );
+}
+
+export { Rodape };
