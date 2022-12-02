3e8ae65023b865732069ae133794d4e7f5b1064e
Jean <jean118877@hotmail.com>
2022-12-02 11:34:11 -0300

    feat: responsivo

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,11 +8,11 @@ function Login() {
   return (
     <>
       <Grid container direction='row' justifyContent='center' alignItems='center'>
-        <Grid alignItems='center' xs={6}>
-          <Box paddingX={20} sx={{ my: 10 }}>
+        <Grid alignItems='center' xs={12} sm={9} md={6}>
+          <Box paddingX={10} sx={{ my: 10 }}>
             <form>
               <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-              <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+              <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
               <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
               <Box marginTop={2} textAlign='center'>
                 <Link to='/home' className='text-decorator-none'>
@@ -26,7 +26,11 @@ function Login() {
               <Box marginRight={1}>
                 <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
               </Box>
-              <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+              <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>
+                <Link to="/cadastrarusuario">
+                  Cadastre-se
+                </Link>
+                </Typography>
             </Box>
           </Box>
         </Grid>
