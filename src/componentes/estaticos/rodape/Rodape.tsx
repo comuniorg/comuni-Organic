8a0fad309b9eb74158e28eb1fab1d2751df5865c
Jean <jean118877@hotmail.com>
2022-12-02 11:07:13 -0300

    fix: rodape

--- src/componentes/estaticos/rodape/Rodape.tsx
+++ src/componentes/estaticos/rodape/Rodape.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import github from '../../../assets/images/github.svg';
 import discord from '../../../assets/images/discord.svg';
 import "./Rodape.css";
-import { classes } from './styles';
 
 function Rodape(){
     return(
       <>
-        <Grid className={classes.footer} container direction="row" justifyContent="space-evenly">
+        <Grid container direction="row" justifyContent="space-evenly">
           <Grid item container xs={6} justifyContent="space-evenly">
             <Grid item>
               <a href="#">
--- src/componentes/estaticos/rodape/styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core";
-
-const styles = makeStyles(theme => ({
-  footer: {
-    background: theme.palette.primary.main
-  }
-}));
-
-export const classes = styles();
