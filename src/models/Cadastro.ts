a6d6b7bd0ef27afa528347e7f1a58975600cad63
Murilo Nascimento <murilojesus659@gmail.com>
2022-12-02 12:17:18 -0300

    feat: add cadastro usuario

---
+++ src/models/Cadastro.ts
@@ -0,0 +1,9 @@
+interface Cadastro {
+    id: number,
+    nome: string,
+    usuario: string,
+    senha: string,
+    confirmarSenha: string
+}
+
+export default Cadastro;
---
+++ src/models/Usuario.ts
@@ -0,0 +1,8 @@
+interface Usuario {
+    id: number,
+    nome: string,
+    usuario: string,
+    senha: string
+}
+
+export default Usuario;
