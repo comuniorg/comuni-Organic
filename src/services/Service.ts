af83be6eda0438989632aede6ab2cf03ce841f0d
Jean <jean118877@hotmail.com>
2022-12-06 10:12:55 -0300

    fix: service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
+import UsuarioLogin from '../models/UsuarioLogin';
 
 export const api = axios.create({
 	baseURL: 'https://db-organica-tgxd.onrender.com'
 });
 
-export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
+export const cadastroUsuario = async (url: string, dados: {nome: string, usuario: string, senha: string}, setDado: React.Dispatch<React.SetStateAction<UsuarioLogin>>) => {
 	const resposta = await api.post(url, dados)
 	setDado(resposta.data)
 }
 
-export const login = async (url: any, dados: any, setDado: any) => {
+export const login = async (url: string, dados: {usuario: string, senha: string}, setDado: React.Dispatch<string>) => {
 	const resposta = await api.post(url, dados)
 	setDado(resposta.data.token)
 }
