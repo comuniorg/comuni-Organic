5b630c57ce1875710ecbf727a0189971a037d3ea
Nicole Moreira <111883973+NicoleMdaSilva@users.noreply.github.com>
2022-12-06 09:52:11 -0300

    fix: typagens das funções

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,12 +4,12 @@ export const api = axios.create({
 	baseURL: 'https://db-organica-tgxd.onrender.com'
 });
 
-export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
+export const cadastroUsuario = async (url: string, dados: {nome: string, usuario: string, senha: string}, setDado: React.Dispatch<string>) => {
 	const resposta = await api.post(url, dados)
 	setDado(resposta.data)
 }
 
-export const login = async (url: any, dados: any, setDado: any) => {
+export const login = async (url: string, dados: {usuario: string, senha: string}, setDado: React.Dispatch<string>) => {
 	const resposta = await api.post(url, dados)
 	setDado(resposta.data.token)
 }
