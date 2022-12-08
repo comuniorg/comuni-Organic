d4215ca65112d0d7309b321df1a2ad4930d88fa5
Murilo Nascimento <murilojesus659@gmail.com>
2022-12-08 16:03:16 -0300

    feat: design navbar

---
+++ src/componentes/estaticos/drawer/Drawer.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+type Anchor = 'right';
+
+export default function SDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor: Anchor, open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor: Anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'right',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      {(['right'] as Anchor[]).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -14,11 +14,21 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import "./Navbar.css";
 import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
+import SearchIcon from '@material-ui/icons/Search';
+import SDrawer from '../drawer/Drawer';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     grow: {
       flexGrow: 1,
+      backgroundColor: '#FEFAE0',
+    },
+    toolbar: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    searching: {
+      marginRight: theme.spacing(2),
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -36,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
       '&:hover': {
         backgroundColor: alpha(theme.palette.common.white, 0.25),
       },
-      marginRight: theme.spacing(2),
+      marginRight: theme.spacing(3),
       marginLeft: 0,
       width: '100%',
       [theme.breakpoints.up('sm')]: {
@@ -176,46 +186,17 @@ export default function Navbar() {
   }
 
   return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
+    <>
+      <AppBar className={classes.grow} position="static">
+        <Toolbar className={classes.toolbar}>
+          <IconButton className={classes.searching} aria-label="search" color="inherit">
+            <SearchIcon />
           </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Comunidade organica
-          </Typography>
-
-          <IconButton>
-            <Link to="/home">
-              <p> Home </p>
-            </Link>
-          </IconButton>
-
-          <IconButton>
-            <Link to="/produtos">
-              <p> Produtos </p>
-            </Link>
-          </IconButton>
-
-          <IconButton>
-            <Link to="/sobre">
-              <p> Sobre </p>
-            </Link>
-          </IconButton>
-          
-          <IconButton onClick={goLogout}>
-            <p> Logout </p>
-          </IconButton>
+          <SDrawer />
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
       {renderMenu}
-    </div>
+    </>
   );
 }
