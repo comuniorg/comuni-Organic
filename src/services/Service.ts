7a5917e2a2e6deb626efcac0eae587c5c4c0583d
NicoleMdaSilva <pauloskx9004@gmail.com>
2022-12-14 11:30:57 -0300

    fix: functions types service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,8 +4,10 @@ import Produto from '../models/Produto';
 import UsuarioLogin from '../models/UsuarioLogin';
 
 export const api = axios.create({
-	baseURL: 'https://db-organica-tgxd.onrender.com'
+	baseURL: 'http://localhost:4000'
+	//baseURL: 'https://db-organica-tgxd.onrender.com'
 });
+
 
 export const cadastroUsuario = async (url: string, dados: {nome: string, usuario: string, senha: string}, setDado: React.Dispatch<React.SetStateAction<UsuarioLogin>>) => {
 	const resposta = await api.post(url, dados)
@@ -22,7 +24,8 @@ export const busca = async(url: string, setDado: React.Dispatch<React.SetStateAc
 	setDado(resposta.data)
 }
 
-export const buscaId = async (url: string, setDado: React.Dispatch<React.SetStateAction<Produto>> | React.Dispatch<React.SetStateAction<Categoria>> | React.Dispatch<React.SetStateAction<Produto | undefined>>, header: { headers: { Authorization: string } }) => {
+export const buscaId = async (url: string, setDado: React.Dispatch<React.SetStateAction<Produto>> | React.Dispatch<React.SetStateAction<Categoria>> |
+	React.Dispatch<React.SetStateAction<Produto | undefined>> | React.Dispatch<React.SetStateAction<Categoria | undefined>>, header: { headers: { Authorization: string } }) => {
 	const resposta = await api.get(url, header)
 	setDado(resposta.data)
 }
@@ -31,12 +34,12 @@ export const deleteId = async (url: string, header: { headers: { Authorization: 
 	const resposta = await api.delete(url, header)
 }
 
-export const post = async (url:string, dados: Produto, setDado: React.Dispatch<React.SetStateAction<Produto>>, header: { headers: { Authorization: string } }) =>{
+export const post = async (url:string, dados: Produto | Categoria, setDado: React.Dispatch<React.SetStateAction<Produto>> | React.Dispatch<React.SetStateAction<Categoria>>, header: { headers: { Authorization: string } }) =>{
 	const resposta = await api.post(url,dados,header)
 	setDado(resposta.data)
 }
 
-export const put = async (url:string, dados: Produto, setDado: React.Dispatch<React.SetStateAction<Produto>>, header: { headers: { Authorization: string } }) =>{
+export const put = async (url:string, dados: Produto | Categoria, setDado: React.Dispatch<React.SetStateAction<Produto>> | React.Dispatch<React.SetStateAction<Categoria>>, header: { headers: { Authorization: string } }) =>{
 	const resposta = await api.post(url,dados,header)
 	setDado(resposta.data)
 }
