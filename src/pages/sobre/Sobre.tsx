9f20f3c5f394fd8163e01bf641a03fe31e0dc677
Caroline Barcelos <cora.carolineb@gmail.com>
2022-12-14 09:30:59 -0300

    the commit: alterações no sobre

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,12 +1,13 @@
 import { Box, Button, Grid, Link, Typography } from '@material-ui/core';
 import React from 'react';
 import './Sobre.css';
-import Carol from './imagens/Caroline.png';
+import Carol from './imagens/Carol.png';
 import Daniel from './imagens/Daniel.png';
 import Murilo from './imagens/Murilo.png';
 import Nicole from './imagens/Nicole.png';
 import Maycon from './imagens/Maycon.png';
-import Hugo from './imagens/Hugo.jpg';
+import Hugo from './imagens/Hugo.png';
+import Jean from './imagens/Jean.png';
 
 
 
@@ -14,27 +15,51 @@ function Sobre() {
 
   return (
     <>
-      <h1>Sobre Nós</h1>
+      <h1></h1>
       <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
         <Grid alignItems="center" item xs={6}>
           <Box paddingX={20}>
             <Typography variant="h5" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Quem Somos?</Typography>
             <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Somos um e-commerce criado para facilitar a comunicação e comércio entre Agricultores Familiares e Associações de comunidades carentes do estado do RJ. </Typography>
             <br />
+            <br />
             <Typography variant="h5" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Missão</Typography>
             <Typography variant="h6" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>A plataforma foi desenvolvida alinhada com a cartilha de Objetivos de Desenvolvimento Sustentável da ONU, mais especificamente com a ODS 2 - Fome Zero e Agricultura Sustentável. Desta forma, visa acessibilizar a alimentação orgânica para comunidades carentes e fomentar a economia local. </Typography>
             <br />
+            <br />
             <Typography variant="h5" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Nossos Colaboradores</Typography>
-            <Grid item xs={2}>
-              <img src={Carol} alt="" width='274px'/>
-              <img src={Daniel} alt="" width='300px'/>
-              <img src={Maycon} alt="" width='300px'/>
-              <img src={Nicole} alt="" width='300px'/>
-              <img src={Murilo} alt="" width='300px'/>
-              <img src={Hugo} alt="" width='300px'/>
-            </Grid>
+            <br />
           </Box>
         </Grid>
+        <Grid item container xs={12} >
+              <a href='https://www.linkedin.com/in/caroline-barcelos-valente-42091a15a/' target='_blank'>
+              <img src={Carol} alt="" width='180px' />
+              </a>
+
+              <a href='https://www.linkedin.com/in/danielcorrea5/' target='_blank'>
+              <img src={Daniel} alt="" width='180px'/>
+              </a>
+
+              <a href='https://www.linkedin.com/in/maycon-silva-4673ba234/' target='_blank'>
+              <img src={Maycon} alt="" width='180px' />
+              </a>
+
+              <a href='https://www.linkedin.com/in/nicole-moreira-da-silva-4aa171234/' target='_blank'>
+              <img src={Nicole} alt="" width='180px' />
+              </a>
+
+              <a href='https://www.linkedin.com/in/murilo-nascimento-0388b4249/' target='_blank'>
+              <img src={Murilo} alt="" width='180px'/>
+              </a>
+
+              <a href='https://www.linkedin.com/in/hugo-ramos-84a76924a/' target='_blank'>
+              <img src={Hugo} alt="" width='180px'/>
+              </a>
+              
+              <a href='https://www.linkedin.com/in/jeangs/' target='_blank'>
+              <img src={Jean} alt="" width='180px'/>
+              </a>
+            </Grid>
       </Grid>
     </>
   )
